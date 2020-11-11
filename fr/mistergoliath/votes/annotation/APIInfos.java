5315db9fc0e064b27523a683344df4f3dbe0e2bc
mistergoliath <mistergoliath41@gmail.com>
2020-11-11 23:32:28 +0100

    Delete APIInfos.java

--- fr/mistergoliath/votes/annotation/APIInfos.java
+++
@@ -1,9 +0,0 @@
-package fr.mistergoliath.votes.annotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface APIInfos {
-	String baseUrl() default "";
-}
